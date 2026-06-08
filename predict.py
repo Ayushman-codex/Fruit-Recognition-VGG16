9b20a33e2562062227071e764f273fce3f2e97c8
Ayushman Gupta <aygupta5869@gmail.com>
2026-06-08 06:40:23 +0530

    Add trained model, outputs, and improved code

--- predict.py
+++ predict.py
@@ -1,34 +1,45 @@
+
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras.preprocessing import image
-
+ 
 model = tf.keras.models.load_model("models/fruit_vgg16_model.h5")
-
+ 
+class_names = [
+    "Apple Braeburn",
+    "Avocado",
+    "Banana",
+    "Cherry",
+    "Grape",
+    "Guava",
+    "Kiwi",
+    "Lemon",
+    "Lychee",
+    "Mango",
+    "Papaya",
+    "Pineapple",
+    "Pomegranate",
+    "Strawberry",
+    "Watermelon"
+]
+ 
 img_path = "test.jpg"
-
-img = image.load_img(img_path, target_size=(224,224))
+ 
+img = image.load_img(img_path, target_size=(224, 224))
 img_array = image.img_to_array(img)
 img_array = np.expand_dims(img_array, axis=0)
 img_array = img_array / 255.0
-
+ 
 prediction = model.predict(img_array)
-
-class_names = [
-    "Apple Braeburn 1",
-    "Avocado 1",
-    "Banana 1",
-    "Cherry 1",
-    "Grape 1",
-    "Guava 1",
-    "Kiwi 1",
-    "Lemon 1",
-    "Lychee 1",
-    "Mango 1",
-    "Papaya 1",
-    "Pineapple 1",
-    "Pomegranate 1",
-    "Strawberry 1",
-    "Watermelon 1"
-]
-
-print("Predicted Fruit:", class_names[np.argmax(prediction)])
+ 
+predicted_index = np.argmax(prediction)
+predicted_fruit = class_names[predicted_index]
+confidence = prediction[0][predicted_index] * 100
+ 
+print("Predicted Fruit:", predicted_fruit)
+print("Confidence:", round(confidence, 2), "%")
+ 
+print("\nTop 3 Predictions:")
+top3 = np.argsort(prediction[0])[::-1][:3]
+for i, idx in enumerate(top3):
+    print(str(i + 1) + ".", class_names[idx], "-", round(prediction[0][idx] * 100, 2), "%")
